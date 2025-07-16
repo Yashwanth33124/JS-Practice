31ef7ab36737f974d4f516348f113aa8dd9218b6
Yashwanth Kumar <cheruvuyashwanth99@gmail.com>
2025-07-16 19:00:44 +0530

    added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("HElloo")
+
+
+// Variables in js
+//in js,variables are used to store and manipulate data.they serves as named container
+// types of variables
+//var
+//let
+//const
+
+
